bfa941c5cbde1e320ba736ad704a79a0b668eac0
Mateus Almeida <mateusalmeida071@icloud.com>
2020-05-29 23:30:14 -0300

    Initial commit

---
+++ database/migrations/2020_05_29_202755_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->enum('type', ['I', 'O', 'T']);
+            $table->double('amount', 10, 2);
+            $table->double('total_before', 10, 2);
+            $table->double('total_after', 10, 2);
+            $table->integer('user_id_transaction')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}

c5b9b60bda581f7a99611b740b5f23b539784775
Mateus Almeida <mateusalmeida071@icloud.com>
2020-05-30 22:43:32 -0300

    Added Controllers

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([ 'middleware' => ['auth'], 'namespace' => 'Admin'], function () {
+	Route::get('admin', 'AdminController@index')->name('admin.home');
 });
+
+Route::get('/', 'Site\SiteController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
